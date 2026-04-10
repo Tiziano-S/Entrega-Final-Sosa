e5a92814df3b451fb26337f8a89faddc0fda9499
Tiziano <tizianoiansosa2012@gmail.com>
2026-04-10 17:51:09 -0300

    *

--- Proyecto-final/Proyecto_final/settings.py
+++ Proyecto-final/Proyecto_final/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5g7ih&(1ni0v%4o!n+y)03fgvf@(#h(8y31wsfkgm)2^!21%lg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
