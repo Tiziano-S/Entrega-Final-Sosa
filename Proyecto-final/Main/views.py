c7b907d9f4914c19f1fd1cb11617a09ae01104a7
Tiziano <tizianoiansosa2012@gmail.com>
2026-04-10 17:46:00 -0300

    httpresponse

--- Proyecto-final/Main/views.py
+++ Proyecto-final/Main/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "Main/index.html")
+
+from django.http import HttpResponse
+
+def test(request):
+    return HttpResponse("OK")
